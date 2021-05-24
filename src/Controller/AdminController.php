08393d0779fb3c378c02bd7ec142da82ca4aed01
Léo Chastagnac <65346336+Chastagnac@users.noreply.github.com>
2021-05-24 12:30:02 +0200

    Gestion des comptes utilisateurs

---
+++ src/Controller/AdminController.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Form\EditUserType;
+use App\Repository\UserRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/admin", name="admin_")
+ */
+class AdminController extends AbstractController
+{
+    /**
+     * @Route("/", name="accueil")
+     */
+    public function index(): Response
+    {
+        return $this->render('admin/index.html.twig', [
+            'controller_name' => 'AdminController',
+        ]);
+    }
+
+    /**
+     * Lister les utilisateurs du site
+     * 
+     * @Route("/utilisateurs", name="utilisateurs")
+     */
+    public function usersList(UserRepository $users)
+    {
+        return $this->render('admin/users.html.twig', [
+            'users' => $users->findAll(),
+        ]);
+    }
+
+    /**
+     * Permet de modifier le statu d'un utilisateur
+     * 
+     * @Route("/utilisateurs/modifier/{id}", name="modifier_utilisateur")
+     */
+    public function editUser(User $user, Request $request)
+    {
+        $form = $this->createForm(EditUserType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            $this->addFlash('message', 'Utilisateur modifié avec succès');
+            return $this->redirectToRoute('admin_utilisateurs');
+        }
+
+        return $this->render('admin/edituser.html.twig', [
+            'userForm' => $form->createView(),
+        ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            // Configure your form options here
+        ]);
+    }
+}
